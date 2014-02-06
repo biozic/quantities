78c8cdf0d8972ea9b78fff5d6bb64f3514f2ba9a
Nicolas Sicard <dransic@gmail.com>
2014-02-06 22:34:15 +0100

    Formatting

--- examples/binarysize.d
+++ examples/binarysize.d
@@ -8,14 +8,26 @@ import std.stdio;
 enum bit = unit!("bit", BigInt);
 enum byte_ = 8 * bit;
 
-alias tera = prefix!(BigInt(1000)^^4); 
+alias tera = prefix!(BigInt(1000)^^4);
 alias tebi = prefix!(BigInt(1024)^^4);
+alias giga = prefix!(BigInt(1000)^^3);
+alias gibi = prefix!(BigInt(1024)^^3);
+alias mega = prefix!(BigInt(1000)^^2);
+alias mebi = prefix!(BigInt(1024)^^2);
+alias kilo = prefix!(BigInt(1000));
+alias kibi = prefix!(BigInt(1024));
 
 enum symbolList = makeSymbolList!BigInt(
     addUnit("bit", bit),
     addUnit("B", byte_),
     addPrefix("T", BigInt(1000)^^4),
-    addPrefix("Ti", BigInt(1024)^^4)
+    addPrefix("Ti", BigInt(1024)^^4),
+    addPrefix("G", BigInt(1000)^^3),
+    addPrefix("Gi", BigInt(1024)^^3),
+    addPrefix("M", BigInt(1000)^^2),
+    addPrefix("Mi", BigInt(1024)^^2),
+    addPrefix("k", BigInt(1000)),
+    addPrefix("Ki", BigInt(1024))
 );
 
 alias bin = ctQuantityParser!(BigInt, (ref string s) => parse!long(s), SymbolList);
@@ -26,9 +38,4 @@ unittest
     auto sizebi = 1 * tebi(byte_);
     writefln("1 TB = %s B", size.value(byte_));
     writefln("1 TiB = %s B", sizebi.value(byte_));
-}
-
-unittest
-{
-    
 }
