a6197dccd3ee877cd1b482aba9537ec91727758d
Per Nordlöw <per.nordlow@gmail.com>
2015-07-12 14:55:58 +0200

    Compilation and tests pass

--- source/quantities/si.d
+++ source/quantities/si.d
@@ -33,114 +33,116 @@ import core.time : Duration, dur;
 
 version (unittest) import std.math : approxEqual;
 
-version (SIReal)
-    alias Numeric = real;
-else version (SIDouble)
-    alias Numeric = double;
-else version (SIFloat)
-    alias Numeric = float;
-else
-    alias Numeric = double;
- 
+// version (SIReal)
+//     alias N = real;
+// else version (SIDouble)
+//     alias N = double;
+// else version (SIFloat)
+//     alias N = float;
+// else
+//     alias N = double;
+
+alias StdN = double; // standard numeric (precision)
+
 /++
 Predefined SI units.
 +/
-enum meter = unit!(Numeric, "L");
+enum meter(N = StdN) = unit!(N, "L");
 alias metre = meter; /// ditto
-enum kilogram = unit!(Numeric, "M"); /// ditto
-enum second = unit!(Numeric, "T"); /// ditto
-enum ampere = unit!(Numeric, "I"); /// ditto
-enum kelvin = unit!(Numeric, "Θ"); /// ditto
-enum mole = unit!(Numeric, "N"); /// ditto
-enum candela = unit!(Numeric, "J"); /// ditto
+enum kilogram(N = StdN) = unit!(N, "M"); /// ditto
+enum second(N = StdN) = unit!(N, "T"); /// ditto
+enum ampere(N = StdN) = unit!(N, "I"); /// ditto
+enum kelvin(N = StdN) = unit!(N, "Θ"); /// ditto
+enum mole(N = StdN) = unit!(N, "N"); /// ditto
+enum candela(N = StdN) = unit!(N, "J"); /// ditto
 
-enum radian = meter / meter; // ditto
-enum steradian = square(meter) / square(meter); /// ditto
-enum hertz = 1 / second; /// ditto
-enum newton = kilogram * meter / square(second); /// ditto
-enum pascal = newton / square(meter); /// ditto
-enum joule = newton * meter; /// ditto
-enum watt = joule / second; /// ditto
-enum coulomb = second * ampere; /// ditto
-enum volt = watt / ampere; /// ditto
-enum farad = coulomb / volt; /// ditto
-enum ohm = volt / ampere; /// ditto
-enum siemens = ampere / volt; /// ditto
-enum weber = volt * second; /// ditto
-enum tesla = weber / square(meter); /// ditto
-enum henry = weber / ampere; /// ditto
-enum celsius = kelvin; /// ditto
-enum lumen = candela / steradian; /// ditto
-enum lux = lumen / square(meter); /// ditto
-enum becquerel = 1 / second; /// ditto
-enum gray = joule / kilogram; /// ditto
-enum sievert = joule / kilogram; /// ditto
-enum katal = mole / second; /// ditto
+enum radian(N = StdN) = meter!N / meter!N; // ditto
+enum steradian(N = StdN) = square(meter!N) / square(meter!N); /// ditto
+enum hertz(N = StdN) = 1 / second!N; /// ditto
+enum newton(N = StdN) = kilogram!N * meter!N / square(second!N); /// ditto
+enum pascal(N = StdN) = newton!N / square(meter!N); /// ditto
+enum joule(N = StdN) = newton!N * meter!N; /// ditto
+enum watt(N = StdN) = joule!N / second!N; /// ditto
+enum coulomb(N = StdN) = second!N * ampere!N; /// ditto
+enum volt(N = StdN) = watt!N / ampere!N; /// ditto
+enum farad(N = StdN) = coulomb!N / volt!N; /// ditto
+enum ohm(N = StdN) = volt!N / ampere!N; /// ditto
+enum siemens(N = StdN) = ampere!N / volt!N; /// ditto
+enum weber(N = StdN) = volt!N * second!N; /// ditto
+enum tesla(N = StdN) = weber!N / square(meter!N); /// ditto
+enum henry(N = StdN) = weber!N / ampere!N; /// ditto
+enum celsius(N = StdN) = kelvin!N; /// ditto
+enum lumen(N = StdN) = candela!N / steradian!N; /// ditto
+enum lux(N = StdN) = lumen!N / square(meter!N); /// ditto
+enum becquerel(N = StdN) = 1 / second!N; /// ditto
+enum gray(N = StdN) = joule!N / kilogram!N; /// ditto
+enum sievert(N = StdN) = joule!N / kilogram!N; /// ditto
+enum katal(N = StdN) = mole!N / second!N; /// ditto
 
-enum gram = 1e-3 * kilogram; /// ditto
-enum minute = 60 * second; /// ditto
-enum hour = 60 * minute; /// ditto
-enum day = 24 * hour; /// ditto
-enum degreeOfAngle = PI / 180 * radian; /// ditto
-enum minuteOfAngle = degreeOfAngle / 60; /// ditto
-enum secondOfAngle = minuteOfAngle / 60; /// ditto
-enum hectare = 1e4 * square(meter); /// ditto
-enum liter = 1e-3 * cubic(meter); /// ditto
+enum gram(N = StdN) = 1e-3 * kilogram!N; /// ditto
+enum minute(N = StdN) = 60 * second!N; /// ditto
+enum hour(N = StdN) = 60 * minute!N; /// ditto
+enum day(N = StdN) = 24 * hour!N; /// ditto
+enum degreeOfAngle(N = StdN) = PI / 180 * radian!N; /// ditto
+enum minuteOfAngle(N = StdN) = degreeOfAngle!N / 60; /// ditto
+enum secondOfAngle(N = StdN) = minuteOfAngle!N / 60; /// ditto
+enum hectare(N = StdN) = 1e4 * square(meter!N); /// ditto
+enum liter(N = StdN) = 1e-3 * cubic(meter!N); /// ditto
 alias litre = liter; /// ditto
-enum ton = 1e3 * kilogram; /// ditto
-enum electronVolt = 1.60217653e-19 * joule; /// ditto
-enum dalton = 1.66053886e-27 * kilogram; /// ditto
+enum ton(N = StdN) = 1e3 * kilogram!N; /// ditto
+enum electronVolt(N = StdN) = 1.60217653e-19 * joule!N; /// ditto
+enum dalton(N = StdN) = 1.66053886e-27 * kilogram!N; /// ditto
 
-//enum one = Quantity!(Numeric, Dimensions.init)(1); /// The dimensionless unit 'one'
+//enum one = Quantity!(N, Dimensions.init)(1); /// The dimensionless unit 'one'
 
-alias Length = typeof(meter); /// Predefined quantity type templates for SI quantities
-alias Mass = typeof(kilogram); /// ditto
-alias Time = typeof(second); /// ditto
-alias ElectricCurrent = typeof(ampere); /// ditto
-alias Temperature = typeof(kelvin); /// ditto
-alias AmountOfSubstance = typeof(mole); /// ditto
-alias LuminousIntensity = typeof(candela); /// ditto
+alias Length(N = StdN) = typeof(meter!N); /// Predefined quantity type templates for SI quantities
+alias Mass(N = StdN) = typeof(kilogram!N); /// ditto
+alias Time(N = StdN) = typeof(second!N); /// ditto
+alias ElectricCurrent(N = StdN) = typeof(ampere!N); /// ditto
+alias Temperature(N = StdN) = typeof(kelvin!N); /// ditto
+alias AmountOfSubstance(N = StdN) = typeof(mole!N); /// ditto
+alias LuminousIntensity(N = StdN) = typeof(candela!N); /// ditto
 
-alias Area = typeof(square(meter)); /// ditto
-alias Surface = Area;
-alias Volume = typeof(cubic(meter)); /// ditto
-alias Speed = typeof(meter/second); /// ditto
-alias Acceleration = typeof(meter/square(second)); /// ditto
-alias MassDensity = typeof(kilogram/cubic(meter)); /// ditto
-alias CurrentDensity = typeof(ampere/square(meter)); /// ditto
-alias MagneticFieldStrength = typeof(ampere/meter); /// ditto
-alias Concentration = typeof(mole/cubic(meter)); /// ditto
-alias MolarConcentration = Concentration; /// ditto
-alias MassicConcentration = typeof(kilogram/cubic(meter)); /// ditto
-alias Luminance = typeof(candela/square(meter)); /// ditto
-alias RefractiveIndex = typeof(kilogram); /// ditto
+alias Area(N = StdN) = typeof(square(meter!N)); /// ditto
+alias Surface(N = StdN) = Area!N;
+alias Volume(N = StdN) = typeof(cubic(meter!N)); /// ditto
+alias Speed(N = StdN) = typeof(meter!N/second!N); /// ditto
+alias Acceleration(N = StdN) = typeof(meter!N/square(second!N)); /// ditto
+alias MassDensity(N = StdN) = typeof(kilogram!N/cubic(meter!N)); /// ditto
+alias CurrentDensity(N = StdN) = typeof(ampere!N/square(meter!N)); /// ditto
+alias MagneticFieldStrength(N = StdN) = typeof(ampere!N/meter!N); /// ditto
+alias Concentration(N = StdN) = typeof(mole!N/cubic(meter!N)); /// ditto
+alias MolarConcentration(N = StdN) = Concentration!N; /// ditto
+alias MassicConcentration(N = StdN) = typeof(kilogram!N/cubic(meter!N)); /// ditto
+alias Luminance(N = StdN) = typeof(candela!N/square(meter!N)); /// ditto
+alias RefractiveIndex(N = StdN) = typeof(kilogram!N); /// ditto
 
-alias Angle = typeof(radian); /// ditto
-alias SolidAngle = typeof(steradian); /// ditto
-alias Frequency = typeof(hertz); /// ditto
-alias Force = typeof(newton); /// ditto
-alias Pressure = typeof(pascal); /// ditto
-alias Energy = typeof(joule); /// ditto
-alias Work = Energy; /// ditto
-alias Heat = Energy; /// ditto
-alias Power = typeof(watt); /// ditto
-alias ElectricCharge = typeof(coulomb); /// ditto
-alias ElectricPotential = typeof(volt); /// ditto
-alias Capacitance = typeof(farad); /// ditto
-alias ElectricResistance = typeof(ohm); /// ditto
-alias ElectricConductance = typeof(siemens); /// ditto
-alias MagneticFlux = typeof(weber); /// ditto
-alias MagneticFluxDensity = typeof(tesla); /// ditto
-alias Inductance = typeof(henry); /// ditto
-alias LuminousFlux = typeof(lumen); /// ditto
-alias Illuminance = typeof(lux); /// ditto
-alias CelsiusTemperature = typeof(celsius); /// ditto
-alias Radioactivity = typeof(becquerel); /// ditto
-alias AbsorbedDose = typeof(gray); /// ditto
-alias DoseEquivalent = typeof(sievert); /// ditto
-alias CatalyticActivity = typeof(katal); /// ditto
+alias Angle(N = StdN) = typeof(radian!N); /// ditto
+alias SolidAngle(N = StdN) = typeof(steradian!N); /// ditto
+alias Frequency(N = StdN) = typeof(hertz!N); /// ditto
+alias Force(N = StdN) = typeof(newton!N); /// ditto
+alias Pressure(N = StdN) = typeof(pascal!N); /// ditto
+alias Energy(N = StdN) = typeof(joule!N); /// ditto
+alias Work(N) = Energy!N; /// ditto
+alias Heat(N) = Energy!N; /// ditto
+alias Power(N = StdN) = typeof(watt!N); /// ditto
+alias ElectricCharge(N = StdN) = typeof(coulomb!N); /// ditto
+alias ElectricPotential(N = StdN) = typeof(volt!N); /// ditto
+alias Capacitance(N = StdN) = typeof(farad!N); /// ditto
+alias ElectricResistance(N = StdN) = typeof(ohm!N); /// ditto
+alias ElectricConductance(N = StdN) = typeof(siemens!N); /// ditto
+alias MagneticFlux(N = StdN) = typeof(weber!N); /// ditto
+alias MagneticFluxDensity(N = StdN) = typeof(tesla!N); /// ditto
+alias Inductance(N = StdN) = typeof(henry!N); /// ditto
+alias LuminousFlux(N = StdN) = typeof(lumen!N); /// ditto
+alias Illuminance(N = StdN) = typeof(lux!N); /// ditto
+alias CelsiusTemperature(N = StdN) = typeof(celsius!N); /// ditto
+alias Radioactivity(N = StdN) = typeof(becquerel!N); /// ditto
+alias AbsorbedDose(N = StdN) = typeof(gray!N); /// ditto
+alias DoseEquivalent(N = StdN) = typeof(sievert!N); /// ditto
+alias CatalyticActivity(N = StdN) = typeof(katal!N); /// ditto
 
-alias Dimensionless = typeof(meter/meter); /// The type of dimensionless quantities
+alias Dimensionless(N = StdN) = typeof(meter!N/meter!N); /// The type of dimensionless quantities
 
 /// SI prefixes.
 alias yotta = prefix!1e24;
@@ -165,44 +167,44 @@ alias zepto = prefix!1e-21; /// ditto
 alias yocto = prefix!1e-24; /// ditto
 
 /// A list of common SI symbols and prefixes
-enum siSymbols = SymbolList!Numeric()
-    .addUnit("m", meter)
-    .addUnit("kg", kilogram)
-    .addUnit("s", second)
-    .addUnit("A", ampere)
-    .addUnit("K", kelvin)
-    .addUnit("mol", mole)
-    .addUnit("cd", candela)
-    .addUnit("rad", radian)
-    .addUnit("sr", steradian)
-    .addUnit("Hz", hertz)
-    .addUnit("N", newton)
-    .addUnit("Pa", pascal)
-    .addUnit("J", joule)
-    .addUnit("W", watt)
-    .addUnit("C", coulomb)
-    .addUnit("V", volt)
-    .addUnit("F", farad)
-    .addUnit("Ω", ohm)
-    .addUnit("S", siemens)
-    .addUnit("Wb", weber)
-    .addUnit("T", tesla)
-    .addUnit("H", henry)
-    .addUnit("lm", lumen)
-    .addUnit("lx", lux)
-    .addUnit("Bq", becquerel)
-    .addUnit("Gy", gray)
-    .addUnit("Sv", sievert)
-    .addUnit("kat", katal)
-    .addUnit("g", gram)
-    .addUnit("min", minute)
-    .addUnit("h", hour)
-    .addUnit("d", day)
-    .addUnit("l", liter)
-    .addUnit("L", liter)
-    .addUnit("t", ton)
-    .addUnit("eV", electronVolt)
-    .addUnit("Da", dalton)
+enum siSymbols(N) = SymbolList!N()
+    .addUnit("m", meter!N)
+    .addUnit("kg", kilogram!N)
+    .addUnit("s", second!N)
+    .addUnit("A", ampere!N)
+    .addUnit("K", kelvin!N)
+    .addUnit("mol", mole!N)
+    .addUnit("cd", candela!N)
+    .addUnit("rad", radian!N)
+    .addUnit("sr", steradian!N)
+    .addUnit("Hz", hertz!N)
+    .addUnit("N", newton!N)
+    .addUnit("Pa", pascal!N)
+    .addUnit("J", joule!N)
+    .addUnit("W", watt!N)
+    .addUnit("C", coulomb!N)
+    .addUnit("V", volt!N)
+    .addUnit("F", farad!N)
+    .addUnit("Ω", ohm!N)
+    .addUnit("S", siemens!N)
+    .addUnit("Wb", weber!N)
+    .addUnit("T", tesla!N)
+    .addUnit("H", henry!N)
+    .addUnit("lm", lumen!N)
+    .addUnit("lx", lux!N)
+    .addUnit("Bq", becquerel!N)
+    .addUnit("Gy", gray!N)
+    .addUnit("Sv", sievert!N)
+    .addUnit("kat", katal!N)
+    .addUnit("g", gram!N)
+    .addUnit("min", minute!N)
+    .addUnit("h", hour!N)
+    .addUnit("d", day!N)
+    .addUnit("l", liter!N)
+    .addUnit("L", liter!N)
+    .addUnit("t", ton!N)
+    .addUnit("eV", electronVolt!N)
+    .addUnit("Da", dalton!N)
     .addPrefix("Y", 1e24)
     .addPrefix("Z", 1e21)
     .addPrefix("E", 1e18)
@@ -232,47 +234,77 @@ enum siSymbols = SymbolList!Numeric()
     .addPrefix("Mi", 1024.0^^2)
     .addPrefix("Ki", 1024.0);
 
-private static SymbolList!Numeric _siSymbols;
-private static Parser!Numeric _siParser;
+private static SymbolList!float _siSymbolsFloat;
+private static SymbolList!double _siSymbolsDouble;
+private static SymbolList!real _siSymbolsReal;
+
+private static Parser!float _siParserFloat;
+private static Parser!double _siParserDouble;
+private static Parser!real _siParserReal;
+
 static this()
 {
-    _siSymbols = siSymbols;
-    _siParser = Parser!Numeric(_siSymbols, &std.conv.parse!(Numeric, string));
+    _siSymbolsFloat = siSymbols!float;
+    _siSymbolsDouble = siSymbols!double;
+    _siSymbolsReal = siSymbols!real;
+    _siParserFloat = Parser!float(_siSymbolsFloat, &std.conv.parse!(float, string));
+    _siParserDouble = Parser!double(_siSymbolsDouble, &std.conv.parse!(double, string));
+    _siParserReal = Parser!real(_siSymbolsReal, &std.conv.parse!(real, string));
 }
 
 /// Parses a string for a quantity of type Q at runtime
 Q parseSI(Q)(string str)
     if (isQuantity!Q)
 {
-    return _siParser.parse!Q(str);
+    alias N = Q.valueType;
+    static if (is(N == float))
+    {
+        return _siParserFloat.parse!Q(str);
+    }
+    else static if (is(N == double))
+    {
+        return _siParserDouble.parse!Q(str);
+    }
+    else static if (is(N == real))
+    {
+        return _siParserReal.parse!Q(str);
+    }
+    else
+    {
+        static assert("Unsupported type Quantity Value Type " ~ N.stringof);
+    }
 }
 ///
 @safe unittest
 {
-    auto t = parseSI!Time("90 min");
-    assert(t == 90 * minute);
-    t = parseSI!Time("h");
-    assert(t == 1 * hour);
+    alias N = double;
+    auto t = "90 min".parseSI!(Time!N);
+    assert(t == 90 * minute!N);
+    t = "h".parseSI!(Time!N);
+    assert(t == 1 * hour!N);
 
-    auto v = parseSI!Dimensionless("2");
-    assert(v == (2 * meter) / meter);
+    auto v = "2".parseSI!(Dimensionless!N);
+    assert(v == (2 * meter!N) / meter!N);
 }
 
 /// A compile-time parser with automatic type deduction for SI quantities.
-alias si = compileTimeParser!(Numeric, siSymbols, std.conv.parse!(Numeric, string));
+alias si(N = StdN) = compileTimeParser!(N, siSymbols!N, std.conv.parse!(N, string));
 ///
 pure @safe unittest
 {
-    enum min = si!"min";
-    enum inch = si!"2.54 cm";
-    auto conc = si!"1 µmol/L";
-    auto speed = si!"m s^-1";
-    auto value = si!"0.5";
-    
-    static assert(is(typeof(inch) == Length));
-    static assert(is(typeof(conc) == Concentration));
-    static assert(is(typeof(speed) == Speed));
-    static assert(is(typeof(value) == Dimensionless));
+    alias N = double;
+    alias siN = si!N;
+
+    enum min = siN!"min";
+    enum inch = siN!"2.54 cm";
+    auto conc = siN!"1 µmol/L";
+    auto speed = siN!"m s^-1";
+    auto value = siN!"0.5";
+
+    static assert(is(typeof(inch) == Length!N));
+    static assert(is(typeof(conc) == Concentration!N));
+    static assert(is(typeof(speed) == Speed!N));
+    static assert(is(typeof(value) == Dimensionless!N));
 }
 
 /++
@@ -303,13 +335,13 @@ struct SIFormatWrapper(Q)
         struct Wrapper
         {
             private Q quantity;
-            
+
             void toString(scope void delegate(const(char)[]) sink) const
             {
                 auto spec = FormatSpec!char(_fmt);
                 spec.writeUpToNextSpec(sink);
                 auto target = spec.trailing.idup;
-                auto unit = parseSI!Q(target);
+                auto unit = target.parseSI!Q;
                 sink.formatValue(quantity.value(unit), spec);
                 sink(target);
             }
@@ -328,31 +360,34 @@ unittest
 {
     import std.string;
 
-    auto sf = SIFormatWrapper!Speed("%.1f km/h");
-    auto speed = 343.4 * meter/second;
+    alias N = double;
+    auto sf = SIFormatWrapper!(Speed!N)("%.1f km/h");
+    auto speed = 343.4 * meter!N/second!N;
     assert("Speed: %s".format(sf(speed)) == "Speed: 1236.2 km/h");
 }
 
 unittest
 {
-    auto sf = SIFormatWrapper!Speed("%.1f km/h");
-    assert(text(sf(si!"343.4 m/s")) == "1236.2 km/h");
-    assert(text(sf(parseSI!Speed("343.4 m/s"))) == "1236.2 km/h");
+    alias N = double;
+    auto sf = SIFormatWrapper!(Speed!N)("%.1f km/h");
+    alias siN = si!N;
+    assert(text(sf(siN!"343.4 m/s")) == "1236.2 km/h");
+    assert(text(sf("343.4 m/s".parseSI!(Speed!N))) == "1236.2 km/h");
 }
 
 /++
 Convenience function that returns a SIFormatter when the format string is
 known at compile-time.
 +/
-auto siFormatWrapper(string fmt)()
+auto siFormatWrapper(N, string fmt)()
 {
-    enum unit = si!({
-        auto spec = FormatSpec!char(fmt);
-        auto dummy = appender!string;
-        spec.writeUpToNextSpec(dummy);
-        return spec.trailing.idup;
-    }());
-
+    alias siN = si!N;
+    enum unit = siN!({
+            auto spec = FormatSpec!char(fmt);
+            auto dummy = appender!string;
+            spec.writeUpToNextSpec(dummy);
+            return spec.trailing.idup;
+        }());
     return SIFormatWrapper!(typeof(unit))(fmt);
 }
 ///
@@ -360,34 +395,37 @@ unittest
 {
     import std.string;
 
-    auto sf = siFormatWrapper!"%.1f km/h";
-    auto speed = 343.4 * meter/second;
+    alias N = double;
+    auto sf = siFormatWrapper!(N, "%.1f km/h");
+    auto speed = 343.4 * meter!N / second!N;
     assert("Speed: %s".format(sf(speed)) == "Speed: 1236.2 km/h");
 }
 
 /// Converts a quantity of time to or from a core.time.Duration
-Time fromDuration(Duration d) pure @safe
+Time!N fromDuration(N)(Duration d) pure @safe
 {
-    return d.total!"hnsecs" * hecto(nano(second));
+    return d.total!"hnsecs" * hecto(nano(second!N));
 }
 
 /// ditto
-Duration toDuration(Q)(Q quantity) 
+Duration toDuration(Q)(Q quantity)
     if (isQuantity!Q)
 {
     import std.conv;
-    auto hns = quantity.value(hecto(nano(second)));
+    alias N = Q.valueType;
+    auto hns = quantity.value(hecto(nano(second!N)));
     return dur!"hnsecs"(hns.roundTo!long);
 }
 
 ///
 @safe unittest // Durations
 {
+    alias N = double;
     auto d = 4.dur!"msecs";
-    auto t = fromDuration(d);
-    assert(t.value(milli(second)).approxEqual(4));
-    
-    auto t2 = 3.5 * minute;
+    auto t = d.fromDuration!N;
+    assert(t.value(milli(second!N)).approxEqual(4));
+
+    auto t2 = 3.5 * minute!N;
     auto d2 = t2.toDuration;
     auto s = d2.split!("minutes", "seconds")();
     assert(s.minutes == 3 && s.seconds == 30);
